687dad585b035ebc85c0831f6bfc35c6fa38fd0b
Neehar Patil <pluckynumbat@gmail.com>
2025-02-26 21:59:44 +0530

    new game event raised by the level manager when the level data is ready

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -12,4 +12,12 @@ public static class GameEvents
     {
         InputDetectedEvent?.Invoke(inputWorldPosition);
     }
+    
+    // raised by the level manager when level data is loaded from a file and ready
+    public delegate void LevelDataReadyHandler(LevelData levelData);
+    public static event LevelDataReadyHandler LevelDataReadyEvent;
+    public static void RaiseLevelDataReadyEvent(LevelData levelData)
+    {
+        LevelDataReadyEvent?.Invoke(levelData);
+    }
 }
