33a5d8e4ace761f3b3417a0871ae37b810919215
Neehar Patil <pluckynumbat@gmail.com>
2025-06-15 13:20:55 +0530

    added a blank handler to the http get requester for when a level is requested from the server

--- Assets/Scripts/Network/HTTPGetRequester.cs
+++ Assets/Scripts/Network/HTTPGetRequester.cs
@@ -10,4 +10,8 @@ public class HTTPGetRequester : MonoBehaviour
     {
         NetworkEvents.LevelRequestedFromServerEvent -= OnLevelRequestedFromServer;
     }
+
+    private void OnLevelRequestedFromServer()
+    {
+    }
 }
