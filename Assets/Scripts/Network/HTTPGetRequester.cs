f1dc02dd7f4189efbfb6144e955032b1c7da5750
Neehar Patil <pluckynumbat@gmail.com>
2025-06-17 16:12:15 +0530

    send the request for a level to the server in response to a level requested network event

--- Assets/Scripts/Network/HTTPGetRequester.cs
+++ Assets/Scripts/Network/HTTPGetRequester.cs
@@ -14,6 +14,7 @@ public class HTTPGetRequester : MonoBehaviour
 
     private void OnLevelRequestedFromServer()
     {
+        StartCoroutine(GetRequest(DefaultServerURI));
     }
 
    private IEnumerator GetRequest(string uri)
