af3f1043c520112e4906a60600a3dc2737866b0f
Neehar Patil <pluckynumbat@gmail.com>
2025-07-02 17:09:17 +0530

    new coroutine to send a test ping request to the server and respond with a remote test completed event

--- Assets/Scripts/Network/HTTPGetRequester.cs
+++ Assets/Scripts/Network/HTTPGetRequester.cs
@@ -20,6 +20,27 @@ public class HTTPGetRequester : MonoBehaviour
     {
         StartCoroutine(GetLevelRequest(RemoteLevelURI));
     }
+    
+    private IEnumerator PerformTestPing(string uri)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+        yield return webRequest.SendWebRequest();
+
+        bool success = false;
+        switch (webRequest.result)
+        {
+            case UnityWebRequest.Result.Success:
+                success = true;
+                Debug.Log("Test Ping Success!");
+                break;
+           
+            default:
+                Debug.LogError("Test Ping Error: " + webRequest.error);
+                break;
+        }
+        
+        NetworkEvents.RaiseRemoteTestCompletedEvent(success);
+    }
 
    private IEnumerator GetLevelRequest(string uri)
    {
